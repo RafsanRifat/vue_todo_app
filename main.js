aa39dbea93024bcf2af5578130a324142cdbb508
RafsanRifat <rafsan.rifat255@gmail.com>
2022-05-21 13:38:03 +0600

    todo adding

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-
-
 const app = Vue.createApp({
-    data(){
-        return{
-            todos : [
-                {id:1, name: 'This is the first todo'},
-                {id:2, name: 'This is the second todo'},
-                {id:3, name: 'This is the third todo'},
-                {id:4, name: 'This is the fourth todo'}
-            ]
+    data() {
+        return {
+            todos: [],
+            newTodo: ''
+        }
+    },
+    methods: {
+        addTodo() {
+
+            return this.todos.push(this.newTodo)
         }
     }
 })
